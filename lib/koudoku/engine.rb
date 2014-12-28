79af159d7a2291dfe18a54453ccd877919d48c65
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-12-28 07:06:48 -0700

    Added guards to create_plans initializer (to attempt) to run only on rails s/foreman start

--- lib/koudoku/engine.rb
+++ lib/koudoku/engine.rb
@@ -17,23 +17,24 @@ module Koudoku
     end
 
     initializer 'kouduku.create_plans' do |app|
-      if Koudoku.create_plans_in_stripe?
-        ::Plan.all.each do |plan|
-          begin
-            puts "PLAN ===> #{plan.inspect}"
-            stripe_plan = Stripe::Plan.retrieve(plan.stripe_id)
-            puts "STRIPE_RESPONSE ==>  #{stripe_plan.inspect}"
-          rescue Stripe::InvalidRequestError => ire
-            puts "#{ire} ==> #{ire.message}"
-            if ire.message == "No such plan: #{plan.stripe_id}"
-              puts "Ok, creating plan ==> #{plan.stripe_id}"
-              Stripe::Plan.create(
-              :amount => plan.price.to_i,
-              :interval => plan.interval,
-              :name => plan.name,
-              :currency => 'usd',
-              :id => plan.stripe_id
-              )
+      if Koudoku.create_plans_in_stripe? && !defined?(::Bundler) and !defined?(::Rake)
+        if ::Plan.defined?
+          ::Plan.all.each do |plan|
+            begin
+              puts "PLAN ===> #{plan.inspect}"
+              stripe_plan = Stripe::Plan.retrieve(plan.stripe_id)
+            rescue Stripe::InvalidRequestError => ire
+              puts "#{ire} ==> #{ire.message}"
+              if ire.message == "No such plan: #{plan.stripe_id}"
+                puts "Ok, creating plan ==> #{plan.stripe_id}"
+                Stripe::Plan.create(
+                :amount => plan.price.to_i,
+                :interval => plan.interval,
+                :name => plan.name,
+                :currency => 'usd',
+                :id => plan.stripe_id
+                )
+              end
             end
           end
         end
