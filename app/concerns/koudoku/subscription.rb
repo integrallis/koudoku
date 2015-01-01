aed5e5588b8d3ea3e1896850afc12a260bfed049
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-12-31 18:31:35 -0700

    Free plan transition

--- app/concerns/koudoku/subscription.rb
+++ app/concerns/koudoku/subscription.rb
@@ -28,6 +28,7 @@ module Koudoku::Subscription
           customer = Stripe::Customer.retrieve(stripe_id)
 
           puts "Customer ==> #{customer.inspect}"
+          puts "Credit Card Token ==> #{credit_card_token}"
 
           if last_four.nil? && !credit_card_token.nil?
             puts "Last four not present... credit_card_token ==> #{credit_card_token}"
